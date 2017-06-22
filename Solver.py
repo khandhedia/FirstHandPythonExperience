a837730ff55f004cc7793c564b7974f99e7ef6de
nirk0816 <niravkhandhedia043126@gmail.com>
2017-06-22 18:10:10 +0530

    First Program

---
+++ Solver.py
@@ -0,0 +1,9 @@
+class Solver:
+    def solve(self):
+        a = int(input("Input A: "))
+        b = int(input("Input B: "))
+        c = int(input("Input C: "))
+        answer = a + b + c
+        print(answer)
+
+Solver().solve()
